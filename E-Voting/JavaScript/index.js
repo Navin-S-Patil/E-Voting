a55fd475535bb15a1de561f1dac47e32cca9ec8a
Navin <coolmaster1881@gmail.com>
2021-11-15 00:46:47 +0530

    init

---
+++ E-Voting/JavaScript/index.js
@@ -0,0 +1,41 @@
+// var number1 = 19;
+// var number2 = 13;
+// console.log(number1+number2);
+/*
+var str1 = "Hello World";
+var str2 = "HOw are You";
+console.log(str1);
+var marks = {
+    patil : 87,
+    Acharya : 87,
+    Navin : 97
+}
+var a = true;
+var b = false;
+
+var arr=[1,2,"patil",3,4]
+
+var x = 13;
+var y = 19;
+console.log(x<=y);
+
+function avg(a,b){
+    return (a+b)/2;
+}
+
+c1= avg(13,19);
+console.log(c1);
+*/
+
+// var a=13;
+// if(a<10){
+//     console.log(a);
+// }
+// else{
+//     console.log(a, "Is not big");
+// }
+
+var arr=[1,2,3,4,5,6,7,8];
+arr.forEach(function(element){
+    console.log(element);
+})
